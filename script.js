6de9345839047bc31f75d1fa3f5c4d182009232c
gugapototski <gugapototski@live.com>
2023-06-08 19:52:45 -0300

    Alteração das merdas do Marcelo

--- script.js
+++ script.js
@@ -5,8 +5,63 @@ function enviarFormulario(event) {
   const email = emailInput.value;
 
   // Lógica para enviar o formulário ou processar os dados
+  const nomeInput = document.getElementById("nome");
+  const nome = nomeInput.value;
 
-  // Exemplo de exibição de mensagem de sucesso
-  alert(`Formulário enviado com sucesso para o e-mail: ${email}`);
+  const tipoServicoSelect = document.getElementById("tipo-servico");
+  const tipoServico = tipoServicoSelect.value;
+
+  const metrosQuadradosInput = document.getElementById("metros-quadrados");
+  const metrosQuadrados = metrosQuadradosInput.value;
+
+  // Validar a quantidade de metros quadrados
+  if (metrosQuadrados <= 0) {
+    alert("A quantidade de metros quadrados deve ser maior que zero.");
+    return; // Encerrar a função se a validação falhar
+  }
+
+  const corDesejadaInput = document.getElementById("cor-desejada");
+  const corDesejada = corDesejadaInput.value;
+
+  const dadosFormulario = {
+    email: email,
+    nome: nome,
+    tipoServico: tipoServico,
+    metrosQuadrados: metrosQuadrados,
+    corDesejada: corDesejada,
+  };
+
+  // Lógica do orçamento
+  const valorServico = 100; // Valor do serviço de pintura por metro quadrado
+
+  const valorEstimadoServico = valorServico * metrosQuadrados;
+
+  // Exibição do orçamento
+  const mensagem = `Orçamento:
+  Nome: ${nome}
+  E-mail: ${email}
+  Tipo de Serviço: ${tipoServico}
+  Metros Quadrados: ${metrosQuadrados}
+  Cor Desejada: ${corDesejada}
+  
+  Valor Estimado de Serviço: R$ ${valorEstimadoServico.toFixed(2)}`;
+
+  // Simulação do envio do formulário
+  console.log("Dados do formulário:", dadosFormulario);
+
+  // Exibição da mensagem de envio para análise
+  alert(
+    `Formulário enviado para análise. Retornaremos para o e-mail: ${email}`
+  );
+
+  // Limpar campos do formulário
   emailInput.value = "";
+  nomeInput.value = "";
+  tipoServicoSelect.value = "";
+  metrosQuadradosInput.value = "";
+  corDesejadaInput.value = "";
+}
+
+function redirect(path) {
+  window.location.href = path;
 }
